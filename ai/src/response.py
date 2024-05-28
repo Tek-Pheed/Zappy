1754e566aa496e71cd71b10c87c8225621b62052
Alexandre <alexandre.ricard@epitech.eu>
2024-05-28 17:21:19 +0200

    FIX: if forest by match case

--- ai/src/response.py
+++ ai/src/response.py
@@ -2,20 +2,21 @@ from typing import Dict, List
 
 def get_inventory(response: str, inv: dict) -> dict:
     for i in range(len(response)):
-        if "food" in response[i]:
-            inv["food"] = int(response[i].split(" ")[2])
-        if "linemate" in response[i]:
-            inv["linemate"] = int(response[i].split(" ")[2])
-        if "deraumere" in response[i]:
-            inv["deraumere"] = int(response[i].split(" ")[2])
-        if "sibur" in response[i]:
-            inv["deraumere"] = int(response[i].split(" ")[2])
-        if "mendiane" in response[i]:
-            inv["mendiane"] = int(response[i].split(" ")[2])
-        if "phiras" in response[i]:
-            inv["phiras"] = int(response[i].split(" ")[2])
-        if "thystame" in response[i]:
-            inv["thystame"] = int(response[i].split(" ")[2])
+        match response[i]:
+            case "food":
+                inv["food"] = int(response[i].split(" ")[2])
+            case "linemate":
+                inv["linemate"] = int(response[i].split(" ")[2])
+            case "deraumere":
+                inv["deraumere"] = int(response[i].split(" ")[2])
+            case "sibur":
+                inv["sibur"] = int(response[i].split(" ")[2])
+            case "mendiane":
+                inv["mendiane"] = int(response[i].split(" ")[2])
+            case "phiras":
+                inv["phiras"] = int(response[i].split(" ")[2])
+            case "thystame":
+                inv["thystame"] = int(response[i].split(" ")[2])
     return inv
 
 def get_case_around_player(response: str) -> dict:
