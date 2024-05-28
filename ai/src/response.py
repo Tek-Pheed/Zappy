3c298e1efbf000b517a4454f32a24bf7cf02a48a
Alexandre <alexandre.ricard@epitech.eu>
2024-05-28 14:53:53 +0200

    FIX: replace class by dictionnary

--- ai/src/response.py
+++ ai/src/response.py
@@ -1,19 +1,21 @@
+from typing import Dict
 from ai.src.data_model import Inventory
 
-def get_inventory(response: str, inv: Inventory) -> Inventory:
+def get_inventory(response: str, inv: dict) -> dict:
+
     for i in range(len(response)):
         if "food" in response[i]:
-            inv = Inventory(int(response[i].split(" ")[2]), inv.linemate, inv.deraumere, inv.sibur, inv.mendiane, inv.phiras, inv.thystame)
+            inv["food"] = int(response[i].split(" ")[2])
         if "linemate" in response[i]:
-            inv = Inventory(inv.food, int(response[i].split(" ")[2]), inv.deraumere, inv.sibur, inv.mendiane, inv.phiras, inv.thystame)
+            inv["linemate"] = int(response[i].split(" ")[2])
         if "deraumere" in response[i]:
-            inv = Inventory(inv.food, inv.linemate, int(response[i].split(" ")[2]), inv.sibur, inv.mendiane, inv.phiras, inv.thystame)
+            inv["deraumere"] = int(response[i].split(" ")[2])
         if "sibur" in response[i]:
-            inv = Inventory(inv.food, inv.linemate, inv.deraumere, int(response[i].split(" ")[2]), inv.mendiane, inv.phiras, inv.thystame)
+            inv["deraumere"] = int(response[i].split(" ")[2])
         if "mendiane" in response[i]:
-            inv = Inventory(inv.food, inv.linemate, inv.deraumere, inv.sibur, int(response[i].split(" ")[2]), inv.phiras, inv.thystame)
+            inv["mendiane"] = int(response[i].split(" ")[2])
         if "phiras" in response[i]:
-            inv = Inventory(inv.food, inv.linemate, inv.deraumere, inv.sibur, inv.mendiane, int(response[i].split(" ")[2]), inv.thystame)
+            inv["phiras"] = int(response[i].split(" ")[2])
         if "thystame" in response[i]:
-            inv = Inventory(inv.food, inv.linemate, inv.deraumere, inv.sibur, inv.mendiane, inv.phiras, int(response[i].split(" ")[2]))
+            inv["thystame"] = int(response[i].split(" ")[2])
     return inv
