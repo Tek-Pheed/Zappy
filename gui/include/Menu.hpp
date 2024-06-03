cfd27cd7682f0e04f7b628bd8994736abfcc8dd7
Arnaud augait <arnaud.augait@epitech.eu>
2024-06-03 17:02:58 +0200

    ADD: generic functions to create an input for ip and port

--- gui/include/Menu.hpp
+++ gui/include/Menu.hpp
@@ -9,6 +9,8 @@
 
 #include <iostream>
 #include "Draw.hpp"
+#include "raylib.h"
+#include <string>
 
 namespace Zappy {
     enum Scene {
@@ -16,9 +18,10 @@ namespace Zappy {
         GAME,
         SETTINGS,
     };
+
 class Menu {
 
-#define MAX_INPUT_CHARS 9
+#define MAX_INPUT_CHARS     9
 
     public:
         Menu();
@@ -31,12 +34,30 @@ class Menu {
         void MainLoop(Model model, Texture2D background, Camera camera, Vector3 position, BoundingBox bounds, Zappy::Draw &draw);
         void UnloadResources(Model model, Texture2D texture_body, Texture2D texture_leaf);
         void LoopForTextbox(Rectangle textBox, bool &mouseOnText, char name[MAX_INPUT_CHARS + 1], int &letterCount, int &framesCounter);
-        void TextBoxForPort(Rectangle textBox, bool mouseOnText, char port[MAX_INPUT_CHARS + 1], int letterCount, int framesCounter);
         void GameScene(Model model, Camera camera, Vector3 position, BoundingBox bounds);
-
+        void TextBoxForIp(Rectangle textBox, bool mouseOnText, char ip[MAX_INPUT_CHARS + 1], int letterCount, int framesCounter);
+        void LoopForTextboxIp(Rectangle textBox, bool &mouseOnText, char ip[MAX_INPUT_CHARS + 1], int &letterCount, int &framesCounter);
+        void TextBoxForPort(Rectangle textBox, bool mouseOnText, char port[MAX_INPUT_CHARS + 1], int letterCount, int framesCounter);
+        void LoopForTextboxPort(Rectangle textBox, bool &mouseOnText, char port[MAX_INPUT_CHARS + 1], int &letterCount, int &framesCounter);
     protected:
     private:
         std::string _host;
         std::string _port;
+};
+
+
+class TextInput {
+    public:
+        TextInput(float x, float y, float width, float height);
+        void UpdateInput();
+        void DrawInput();
+        std::string GetText() const;
+
+    private:
+        Rectangle bounds;
+        std::string text;
+        int letterCount;
+        bool mouseOnText;
+        int framesCounter;
 };
 }
