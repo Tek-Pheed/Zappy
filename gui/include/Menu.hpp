73f0a25cc876a193d3f19fea38800e366eef30f7
Arnaud augait <arnaud.augait@epitech.eu>
2024-06-03 14:54:15 +0200

    ADD: add prototype function for textBox port

--- gui/include/Menu.hpp
+++ gui/include/Menu.hpp
@@ -18,7 +18,7 @@ namespace Zappy {
     };
 class Menu {
 
-#define MAX_INPUT_CHARS     9
+#define MAX_INPUT_CHARS 9
 
     public:
         Menu();
@@ -30,6 +30,9 @@ class Menu {
         void ConfigureCamera(Camera &camera);
         void MainLoop(Model model, Texture2D background, Camera camera, Vector3 position, BoundingBox bounds, Zappy::Draw &draw);
         void UnloadResources(Model model, Texture2D texture_body, Texture2D texture_leaf);
+        void LoopForTextbox(Rectangle textBox, bool &mouseOnText, char name[MAX_INPUT_CHARS + 1], int &letterCount, int &framesCounter);
+        void TextBoxForPort(Rectangle textBox, bool mouseOnText, char port[MAX_INPUT_CHARS + 1], int letterCount, int framesCounter);
+        void GameScene(Model model, Camera camera, Vector3 position, BoundingBox bounds);
 
     protected:
     private:
