277c76ec8d78afc25e45e2701f3d8041f30f0bed
Arnaud augait <arnaud.augait@epitech.eu>
2024-06-03 11:52:32 +0200

    ADD: DEFINE of 9 carac for textBox input. Adding enum for the differents type of scene, and all the prototype of function

--- gui/include/Menu.hpp
+++ gui/include/Menu.hpp
@@ -8,14 +8,28 @@
 #pragma once
 
 #include <iostream>
+#include "Draw.hpp"
 
 namespace Zappy {
+    enum Scene {
+        MENU,
+        GAME,
+        SETTINGS,
+    };
 class Menu {
+
+#define MAX_INPUT_CHARS     9
+
     public:
         Menu();
         ~Menu();
         void setHost(std::string host);
         void setPort(std::string port);
+        bool InitWindowAndResources(int screenWidth, int screenHeight);
+        void LoadResources(Model &model, Texture2D &texture_body, Texture2D &texture_leaf);
+        void ConfigureCamera(Camera &camera);
+        void MainLoop(Model model, Texture2D background, Camera camera, Vector3 position, BoundingBox bounds, Zappy::Draw &draw);
+        void UnloadResources(Model model, Texture2D texture_body, Texture2D texture_leaf);
 
     protected:
     private:
