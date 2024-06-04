7baa03209d6790ddb890b2fa12334bf192d58279
Alexandre <alexandre.ricard@epitech.eu>
2024-06-04 09:01:47 +0200

    MOVE: function in response file to action file

--- ai/src/action.py
+++ ai/src/action.py
@@ -1,26 +1,53 @@
 from ai.src.server import Server
 
-def send_request(server: Server, message: str) -> str:
-    server.send_message(f"{message}\n")
-    res = server.receive_message()
-    if "dead" in res:
-        server.close_connection()
-        exit(0)
-    return res
-
-def create_team(args, server: Server):
-    server.init_connection()
-    server.receive_message()
-    server.send_message(args.n + "\n")
-    server.receive_message()
-
 def get_food(cases: dict, player: dict, server: Server):
     for i in range (len(cases)):
         if "food" in cases[i]:
-            send_request(server, "Take food")
+            server.send_message("Take food\n")
         else:
-            send_request(server, "Forward")
+            server.send_message("Forward\n")
             if i == 1:
-                send_request(server, "Right")
+                server.send_message("Right\n")
             elif i == 3:
-                send_request(server, "Left")
+                server.send_message("Left\n")
+
+def get_inventory(response: str, inv: dict) -> dict:
+    for i in range(len(response)):
+        match response[i]:
+            case "food":
+                inv["food"] = int(response[i].split(" ")[2])
+            case "linemate":
+                inv["linemate"] = int(response[i].split(" ")[2])
+            case "deraumere":
+                inv["deraumere"] = int(response[i].split(" ")[2])
+            case "sibur":
+                inv["sibur"] = int(response[i].split(" ")[2])
+            case "mendiane":
+                inv["mendiane"] = int(response[i].split(" ")[2])
+            case "phiras":
+                inv["phiras"] = int(response[i].split(" ")[2])
+            case "thystame":
+                inv["thystame"] = int(response[i].split(" ")[2])
+    return inv
+
+def get_case_around_player(response: str) -> dict:
+    res = dict()
+
+    for i in range(len(response)):
+        res[i] = response[i]
+    return res
+
+def get_number_of_team_unused(response: str) -> int:
+    return int(response)
+
+def get_OK_KO(response: str) -> bool:
+    if "ok" in response:
+        return True
+    else:
+        return False
+
+def get_incantation(response: str):
+    if "ko" in response:
+        return False
+    else:
+        return response
