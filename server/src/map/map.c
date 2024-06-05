6c144d58ed34c21848ead2caac4986d580394f22
RaphxelS <raphael.scandella@epitech.eu>
2024-06-05 10:50:37 +0200

    FIX: map update

--- server/src/map/map.c
+++ server/src/map/map.c
@@ -5,39 +5,24 @@
 ** map
 */
 
+#include "map.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "commands.h"
 #include "server.h"
 
-static void get_current_quantity(const server_t *serv, float quantity[7])
-{
-    for (int i = 0; i != serv->resX; i++) {
-        for (int j = 0; j != serv->resY; j++) {
-            quantity[LINEMATE] += serv->map[i][j].stone[LINEMATE];
-            quantity[DERAUMERE] += serv->map[i][j].stone[DERAUMERE];
-            quantity[SIBUR] += serv->map[i][j].stone[SIBUR];
-            quantity[MENDIANE] += serv->map[i][j].stone[MENDIANE];
-            quantity[PHIRAS] += serv->map[i][j].stone[PHIRAS];
-            quantity[THYSTAME] += serv->map[i][j].stone[THYSTAME];
-            quantity[6] += serv->map[i][j].food;
-        }
-    }
-}
-
 void calculate_quantity(const server_t *serv, float quantity[7])
 {
     float total = serv->resX * serv->resY;
 
-    get_current_quantity(serv, quantity);
-    quantity[LINEMATE] = (total * LINEMATE_R) - quantity[LINEMATE];
-    quantity[DERAUMERE] = (total * DERAUMERE_R) - quantity[DERAUMERE];
-    quantity[SIBUR] = (total * SIBUR_R) - quantity[SIBUR];
-    quantity[MENDIANE] = (total * MENDIANE_R) - quantity[MENDIANE];
-    quantity[PHIRAS] = (total * PHIRAS_R) - quantity[PHIRAS];
-    quantity[THYSTAME] = (total * THYSTAME_R) - quantity[THYSTAME];
-    quantity[6] = (total * FOOD_R) - quantity[6];
+    quantity[LINEMATE] = (total * LINEMATE_R);
+    quantity[DERAUMERE] = (total * DERAUMERE_R);
+    quantity[SIBUR] = (total * SIBUR_R);
+    quantity[MENDIANE] = (total * MENDIANE_R);
+    quantity[PHIRAS] = (total * PHIRAS_R);
+    quantity[THYSTAME] = (total * THYSTAME_R);
+    quantity[6] = (total * FOOD_R);
 }
 
 static void create_cell(cell_t *cell, int x, int y)
@@ -62,10 +47,8 @@ void distribute_items(
         y = rand() % serv->resY;
         if (item_type == 6) {
             map[x][y].food++;
-            event_tile_update(serv, x, y);
         } else {
             map[x][y].stone[item_type]++;
-            event_tile_update(serv, x, y);
         }
     }
 }
