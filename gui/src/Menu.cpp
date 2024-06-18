2cabcf0ae8767aaa75f44e3a36eee48e9e149b18
LucasLoustalot <lucas.loustalot@epitech.eu>
2024-06-18 17:59:19 +0200

    REVERT: camera change

--- gui/src/Menu.cpp
+++ gui/src/Menu.cpp
@@ -82,8 +82,11 @@ void Zappy::Menu::LoadResources(RessourceManager &objectPool)
 
 void Zappy::Menu::ConfigureCamera(Camera &camera)
 {
-    camera = {0, 0, 0, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 90.0f,
-        CAMERA_PERSPECTIVE};
+    camera.position = (Vector3){0.0f, 2.0f, 25.0f};
+    camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+    camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+    camera.fovy = 90.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
 }
 
 void loadItems(RessourceManager &objectPool)
