ccd6891fbb49b7de8b11f6ee1bb360105b186893
Alexandre <alexandre.ricard@epitech.eu>
2024-06-20 14:37:16 +0200

    FIX: bad import ressource

--- gui/src/Menu.cpp
+++ gui/src/Menu.cpp
@@ -82,7 +82,7 @@ void Zappy::Menu::GameScene(RessourceManager &objectPool, Vector3 position,
     Players listPlayers;
     objectPool.models.loadRessource("water", "assets/water.obj");
     objectPool.models.loadRessource("island", "assets/island.obj");
-    objectPool.models.loadRessource("player", "assets/korok.obj");
+    objectPool.models.loadRessource("player", "assets/makar.obj");
 
     Camera3D camera = {22.0f, 22.0f, 22.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
         45.0f, CAMERA_PERSPECTIVE};
