9e7576e2c92fad43a74fd2e830f66a29be0817d5
Alexandre <alexandre.ricard@epitech.eu>
2024-06-20 10:51:45 +0200

    FIX: compil error

--- gui/include/Menu.hpp
+++ gui/include/Menu.hpp
@@ -39,7 +39,7 @@ namespace Zappy
         void MainLoop(RessourceManager &objectPool, Camera camera, Vector3 position, BoundingBox bounds, Zappy::Draw &draw);
         void UnloadResources(Model model, Texture2D texture_body, Texture2D texture_leaf);
         void LoopForTextbox(Rectangle textBox, bool &mouseOnText, char name[MAX_INPUT_CHARS + 1], int &letterCount, int &framesCounter);
-        void GameScene(Model model, Vector3 position, BoundingBox bounds);
+        void GameScene(RessourceManager &objectPool, Vector3 position, BoundingBox bounds, Zappy::Server server, Music music);
         void TextBoxForIp(Rectangle textBox, bool mouseOnText, char ip[MAX_INPUT_CHARS + 1], int letterCount, int framesCounter);
         void LoopForTextboxIp(Rectangle textBox, bool &mouseOnText, char ip[MAX_INPUT_CHARS + 1], int &letterCount, int &framesCounter);
         void TextBoxForPort(Rectangle textBox, bool mouseOnText, char port[MAX_INPUT_CHARS + 1], int letterCount, int framesCounter);
